b0b171edceb221c8ece8c200a094833675c1f09d
Coxhuang <mhesat@outlook.com>
2019-10-16 00:26:52 +0800

    update
    * {
    				box-sizing: border-box;
    			}
    
    不能随便加其他的属性

--- src/components/views/home/Home.vue
+++ src/components/views/home/Home.vue
@@ -1,50 +1,53 @@
 <style lang="scss" scoped>
-	.my-home-con{
-		box-sizing: border-box;
-		width: 100%;
-		height: 100%;
-		margin: 0;
-		padding: 0;
-		background-color: #ffffff;
-		.my-home-box{
-			width: 100%;
-			height: 100%;
-			margin: 0;
-			padding: 0;
-			background-color: #ffffff;
-		}
-	}
-	.el-container{
-		height: 100%;
-		border: 0 solid #eee;
-	}
-	.el-header{
-		width: 100%;
-		height: 60px;
-		margin: 0;
-		padding: 0;
-	}
+	/*.my-home-con{*/
+	/*	box-sizing: border-box;*/
+	/*	width: 100%;*/
+	/*	height: 100%;*/
+	/*	margin: 0;*/
+	/*	padding: 0;*/
+	/*	background-color: #ffffff;*/
+	/*	.my-home-box{*/
+	/*		width: 100%;*/
+	/*		height: 100%;*/
+	/*		margin: 0;*/
+	/*		padding: 0;*/
+	/*		background-color: #ffffff;*/
+	/*	}*/
+	/*}*/
+	/*.el-container{*/
+	/*	height: 100%;*/
+	/*	border: 0 solid #eee;*/
+	/*}*/
+	/*.el-header{*/
+	/*	width: 100%;*/
+	/*	height: 60px;*/
+	/*	margin: 0;*/
+	/*	padding: 0;*/
+	/*}*/
 </style>
 
 <template>
-	<div class="my-home-con">
-		<div class="my-home-box">
-			<el-container>
-				<el-header>
+<!--	<div class="my-home-con">-->
+<!--		<div class="my-home-box">-->
+<!--			<el-container>-->
+<!--				<el-header style="background-color: #409eff">-->
+<!--				</el-header>-->
 
-				</el-header>
-			</el-container>
-		</div>
-	</div>
+<!--				<el-main style="width: 100%;height: 100%;margin: 0;padding: 0;">-->
+<!--				</el-main>-->
+<!--			</el-container>-->
+<!--		</div>-->
+<!--	</div>-->
 
-
+	<myaside></myaside>
 
 
 </template>
 
 <script>
-	import container from "@/components/views/home/container/Container"
+	import mycontainer from "@/components/views/home/container/Container"
 	import myheader from "@/components/views/home/header/Header"
+	import myaside from "@/components/views/home/aside/Aside"
 
 	export default {
 		name: 'Home',
@@ -55,7 +58,8 @@
 		},
 		components:{
             myheader,
-            container,
+            mycontainer,
+            myaside,
 		}
 }
 </script>
