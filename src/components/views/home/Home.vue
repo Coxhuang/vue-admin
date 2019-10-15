d39cea9b8f75f708f3d41e44541883fee19a6f72
Coxhuang <mhesat@outlook.com>
2019-10-16 00:05:02 +0800

    update

--- src/components/views/home/Home.vue
+++ src/components/views/home/Home.vue
@@ -1,8 +1,44 @@
 <style lang="scss" scoped>
-
+	.my-home-con{
+		box-sizing: border-box;
+		width: 100%;
+		height: 100%;
+		margin: 0;
+		padding: 0;
+		background-color: #ffffff;
+		.my-home-box{
+			width: 100%;
+			height: 100%;
+			margin: 0;
+			padding: 0;
+			background-color: #ffffff;
+		}
+	}
+	.el-container{
+		height: 100%;
+		border: 0 solid #eee;
+	}
+	.el-header{
+		width: 100%;
+		height: 60px;
+		margin: 0;
+		padding: 0;
+	}
 </style>
 
 <template>
+	<div class="my-home-con">
+		<div class="my-home-box">
+			<el-container>
+				<el-header>
+
+				</el-header>
+			</el-container>
+		</div>
+	</div>
+
+
+
 
 </template>
 
